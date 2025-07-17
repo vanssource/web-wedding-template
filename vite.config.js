c5db61886c890e0f08e338d9ba6c1e495b3bbdc1
vanssource <vannessintern.de@gmail.com>
2025-07-17 21:28:52 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/wedding_web_test/", // sesuai nama repo GitHub
+  base: "/web-wedding-template/", // sesuai nama repo GitHub
   plugins: [react()],
 });
